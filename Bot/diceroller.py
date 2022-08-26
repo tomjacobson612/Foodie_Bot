8ae7dfb6e625c0bcf520d4b8cd625c175d8ada07
Tom Jacobson <94944188+tomjacobson612@users.noreply.github.com>
2022-08-26 13:10:32 -0700

    General update.
    Cleaned up formatting and updated dice  roll logic

--- Bot/diceroller.py
+++ Bot/diceroller.py
@@ -1,10 +1,6 @@
 import random
 
-def hello():
-  print('hello')
 
-#need to add random number generation and need to add ability to parse amount and sides from given string
-#can we return an array that is the length of amount, each value is one roll?
 def roll_dice(amount, sides):
   """Takes an amount and # of sides and rolls dice, creating output for each result and a sum.
   Discord limits messages to 2000 characters and must be non-empty, function bounds checks this."""
@@ -24,15 +20,15 @@ def roll_dice(amount, sides):
       output += ' = '
       sum += num
       output += str(sum)
-      #return output
+
       if len(output) > 2000:
-        output = "Fuk u"
+        output = "Explain to me a situation where you'd need to roll this many dice."
         return output
       else:
         return output
       
     else:
-      output += ' '
+      output += ', '
       sum += num
     i += 1
 
@@ -43,8 +39,6 @@ def roll_dice(amount, sides):
   
 def parse_roll(string):
   """Parses valid discord message to find number of rolls and number of sides. This is done by looking for the 'd'   character in the message and slicing the string around it."""
-  amount = 0
-  sides = 0
   d_index = -1
 
   for i in range(len(string)):
--- Bot/main.py
+++ Bot/main.py
@@ -13,29 +13,33 @@ client = discord.Client(intents=intents)
 async def on_ready():
   print("We have logged in as {0.user}".format(client))
 
+
 @client.event
 async def on_message(message):
-  #checks whether the author of the message is the bot, don't want to reply to self.
+  # checks whether the author of the message is the bot, don't want to reply to self.
   if message.author == client.user:
-    return 
-  #if message starts with '$hello', then send 'Hello!'
-  elif message.content.startswith("!hello"):
+    return
+    # checks for specific author id
+
+  """if message.author.id == 100344908467961856:
+    #adds reaction, arg must be valid UNICODE
+    await message.add_reaction('😈')"""
+
+  # if message starts with '$hello', then send 'Hello!'
+  if message.content.startswith("!hello"):
     await message.channel.send("Hello!")
 
-  #uses regex to search message.content for "Paul", ignoring case
-  #Regex string 'Paul', exact match only r'\b(Paul)\b''
+  # uses regex to search message.content for "Paul", ignoring case
+  # Regex string 'Paul', exact match only r'\b(Paul)\b''
   elif re.search(r'\b(Paul)\b', message.content, re.IGNORECASE):
     await message.channel.send("Shut up Paul!")
 
-  #diceroll command
+  # diceroll command
   elif re.search(r'^[!][0-9]{1,5}?[d][0-9]{1,5}?$', message.content):
     await message.channel.send(diceroller.parse_roll(message.content))
 
   elif message.content.startswith("!food"):
     await message.channel.send(foodIdeas.random_food())
 
-  elif message.author.id == 100344908467961856:
-    #await message.channel.send("Tom")
-    await message.add_reaction('😈')
 
 client.run(token)
